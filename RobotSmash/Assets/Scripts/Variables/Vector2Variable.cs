ab3ac755512acd6e63d4099c02e4b9bc46798e1d
Seyk10 <44772331+Seyk10@users.noreply.github.com>
2020-09-22 11:19:33 +0200

    Versión limpia actualizada
    Con últimos scripts de José sin petes

---
+++ RobotSmash/Assets/Scripts/Variables/Vector2Variable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace RobotSmash
+{
+    namespace Variables
+    {
+        [CreateAssetMenu(fileName = "New vector2 variable", menuName ="Variables/Vector2")]
+        public class Vector2Variable : AVariables<Vector2> { }
+    }
+}
